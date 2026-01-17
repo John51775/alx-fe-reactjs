350a87f4e723446f85e4e1e17786e8966f6d6ba9
John Ngugi <ngaruiyajohn86@gmail.com>
2026-01-17 11:37:04 +0300

    add alx-react-app-new

---
+++ alx-react-app-new/src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer () {
+    return (
+        <div>
+            <footer>
+             <p style={{backgroundColor:'navy', height:"100px", color:"white"}}>© 2023 City Lovers</p>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;

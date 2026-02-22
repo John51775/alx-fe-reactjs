69262103959ab284885debfc6940659f3f76d5a9
John Ngugi <ngaruiyajohn86@gmail.com>
2026-02-22 11:33:57 +0300

    Advanced Data Handling with React Query

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PostsComponent from './components/PostsComponent';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; // since you installed @tanstack/react-query
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PostsComponent />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
